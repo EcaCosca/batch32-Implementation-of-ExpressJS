b2da9aff940754bb62709d4fa0690e6f8756cf44
EcaCosca <enriquecoscarelli@gmail.com>
2022-11-17 17:43:16 +0100

    ex 2 done

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 const PORT = 3000;
 
-// get request for showing "hello"
+// GET request for showing "hello"
 app.get('/', (req, res)=>{res.send('hello')})
+
+// PUT request for sending file.html
+app.put('/', (req, res)=>{
+    let options = {
+        root: path.join(__dirname)
+    };
+     
+    var fileName = 'file.html';
+    res.sendFile(fileName, options, function (err) {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
+})
 
 app.listen(PORT,function(){
   console.log('Hello');
